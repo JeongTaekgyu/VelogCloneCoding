77a35116d33a9b34a35d43c9831c86d03d93add1
taekgyu <taekgyu0602@gmail.com>
2022-02-18 14:32:38 +0900

    Initial commit

---
+++ src/main/java/com/example/velogclonecoding/VelogCloneCodingApplication.java
@@ -0,0 +1,13 @@
+package com.example.velogclonecoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VelogCloneCodingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VelogCloneCodingApplication.class, args);
+    }
+
+}
